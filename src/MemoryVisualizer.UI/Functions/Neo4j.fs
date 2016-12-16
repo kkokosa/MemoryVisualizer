15c99b623726809574affcea59bac6eeaabea7ef
Konrad Kokosa <kkokosa@f28-studio.com>
2016-12-16 21:49:27 +0100

    Neo4j server management - first draft

---
+++ src/MemoryVisualizer.UI/Functions/Neo4j.fs
@@ -0,0 +1,32 @@
+﻿module Neo4j
+
+open System.Diagnostics
+
+type Neo4jState = 
+    | Starting
+    | Started of int
+    | Stopping
+    | Stopped
+
+type Neo4j = { 
+    Name : string 
+    State : Neo4jState
+}
+
+let Create name =
+    { Name = name; State = Stopped }
+    
+let start (instance : Neo4j) =
+    printfn "Starting"
+    let pi = new ProcessStartInfo("""..\..\..\..\paket-files\neo4j.com\neo4j-community-3.0.1\bin\neo4j.bat""", "console")
+    let process = Process.Start(pi)
+    { instance with State = Started(process.Id) }
+
+let stop (instance : Neo4j) =
+    match instance.State with
+    | Started id -> printf "Stopping"
+                    let process = Process.GetProcessById(id)
+                    process.CloseMainWindow()
+                    { instance with State = Stopped }
+    | _ -> printf "Error"
+           instance
--- src/MemoryVisualizer.UI/Program.fs
+++ src/MemoryVisualizer.UI/Program.fs
@@ -5,12 +5,18 @@ open System.Windows
 open FsXaml
 
 open ViewModels
+open Neo4j
 
 type MainWindow = XAML<"MainWindow.xaml", true>
 
 [<STAThread>]
 [<EntryPoint>]
 let main argv = 
+    let neo4j = start (Neo4j.Create "Main")
+
     let window = new MainWindow()
     window.Root.DataContext <- new MainViewModel()
-    (new Application()).Run(window.Root)
+    let result = (new Application()).Run(window.Root)
+
+    stop neo4j
+    result
